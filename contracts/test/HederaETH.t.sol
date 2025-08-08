8a9871534fb72c5f909dcc2e188315f0ed16c14d
Luiz <luiz.vasconcelos@openzeppelin.com>
2025-08-08 20:48:01 -0300

    rename

---
+++ contracts/test/HederaETH.t.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+
+contract HederaETFTest is Test {
+
+    function setUp() public {
+    }
+
+    function test_Increment() public {
+
+    }
+}
