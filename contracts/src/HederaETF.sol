63b18c0d107f7d5033ab586a82015ee704901b13
Luiz <luiz.vasconcelos@openzeppelin.com>
2025-08-08 20:26:12 -0300

    register investor and initialize token

---
+++ contracts/src/HederaETF.sol
@@ -0,0 +1,73 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+pragma experimental ABIEncoderV2;
+
+import "./hedera/HederaTokenService.sol";
+import "./hedera/IHederaTokenService.sol";
+import "./hedera/KeyHelper.sol";
+
+import {IERC4626} from "openzeppelin/interfaces/IERC4626.sol";
+import {IERC20} from "openzeppelin/token/ERC20/IERC20.sol";
+import {Ownable} from "openzeppelin/access/Ownable.sol";
+
+contract HederaETF is HederaTokenService, KeyHelper, Ownable {
+    // Hedera token address
+    address private _tokenAddress;
+
+    // ERC4626 tokens
+    IERC4626 private _vault; // USDY
+    IERC20 private _underlyingToken; // USDC
+
+    event TokenCreated(address tokenAddress);
+    event InvestorRegistered(address investor);
+
+    constructor(address owner_, address vaultAddress_, address underlyingTokenAddress_) Ownable(owner_) {
+        _vault = IERC4626(vaultAddress_);
+        _underlyingToken = IERC20(underlyingTokenAddress_);
+
+        _initializeToken();
+    }
+
+    function _initializeToken() internal {
+        IHederaTokenService.HederaToken memory token;
+        token.name = "Hedera ETF - USDY";
+        token.symbol = "HETF-USDY";
+        token.memo = "Hedera ETF - USDY";
+        token.treasury = address(this);
+
+        IHederaTokenService.TokenKey[] memory keys = new IHederaTokenService.TokenKey[](5);
+
+        // Supply key - for minting/burning
+        keys[0] = getSingleKey(KeyType.SUPPLY, KeyValueType.CONTRACT_ID, address(this));
+
+        // Pause key
+        keys[1] = getSingleKey(KeyType.PAUSE, KeyValueType.CONTRACT_ID, address(this));
+
+        // Freeze key
+        keys[2] = getSingleKey(KeyType.FREEZE, KeyValueType.CONTRACT_ID, address(this));
+
+        // Wipe key
+        keys[3] = getSingleKey(KeyType.WIPE, KeyValueType.CONTRACT_ID, address(this));
+
+        // Delete key
+        keys[4] = getSingleKey(KeyType.ADMIN, KeyValueType.CONTRACT_ID, address(this));
+
+        token.tokenKeys = keys;
+
+        (int256 responseCode, address createdTokenAddress) = createFungibleToken(token, 0, 6);
+
+        require(responseCode == HederaResponseCodes.SUCCESS, "Failed to create token");
+
+        _tokenAddress = createdTokenAddress;
+        emit TokenCreated(_tokenAddress);
+    }
+
+    function registerInvestor() public {
+        int256 responseCode = grantTokenKyc(_tokenAddress, msg.sender);
+
+        require(responseCode == HederaResponseCodes.SUCCESS, "Failed to grant KYC");
+        emit InvestorRegistered(msg.sender);
+    }
+
+    
+}
