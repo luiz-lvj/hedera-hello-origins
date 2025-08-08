d8c6fbe6d6212a4a68527ed6ddd10ec76487eb8a
Luiz <luiz.vasconcelos@openzeppelin.com>
2025-08-08 20:53:22 -0300

    test

--- contracts/test/HederaETH.t.sol
+++ contracts/test/HederaETH.t.sol
@@ -3,8 +3,31 @@ pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
 
-contract HederaETFTest is Test {
-    function setUp() public {}
+import {HederaETF} from "../src/HederaETF.sol";
+import {MockERC20} from "../src/mocks/MockERC20.sol";
+import {Vault} from "../src/Vault.sol";
 
-    function test_Increment() public {}
+contract HederaETFTest is Test {
+    MockERC20 public usdc;
+    Vault public vaultUsdY;
+    HederaETF public hederaETF;
+
+    address public owner = makeAddr("owner");
+
+    address public user = makeAddr("user");
+
+    address public nonRegisteredUser = makeAddr("nonRegisteredUser");
+
+    function setUp() public {
+        usdc = new MockERC20("USDC", "USDC");
+        vaultUsdY = new Vault(usdc, "USDY", "USDY");
+
+        hederaETF = new HederaETF(address(this), address(vaultUsdY), address(usdc));
+
+        usdc.mint(user, 1000 ether);
+    }
+
+    function test_registerInvestor() public {
+        hederaETF.registerInvestor();
+    }
 }
